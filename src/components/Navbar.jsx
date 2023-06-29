c6027b268b80b843acdc99fe8ba76c0b599de777
Auzan <istikhoriauzan@gmail.com>
2023-06-30 00:29:03 +0700

    fix: dropdown menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
             onClick={() => setShow(!show)}
             className={`${
               show ? "block" : ""
-            } flex bg-[#D2E4F7] p-2 rounded-md font-medium gap-2 lg:hidden text-primary hover:text-gray-700 focus:text-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500`}
+            } flex bg-[#D2E4F7] p-2 rounded-md font-medium gap-2 lg:hidden text-primary hover:text-gray-700`}
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -39,22 +39,18 @@ const Navbar = () => {
             </svg>
             Menu
           </button>
-          <div
-            id="menu"
-            className={` ${show ? "" : "hidden"} lg:block `}
-          >
+          <div id="menu" className={` ${show ? "" : "hidden"} lg:block `}>
             <button
               onClick={() => setShow(!show)}
-              className={`block lg:hidden text-gray-500 hover:text-gray-700 focus:text-gray-700 fixed focus:outline-none focus:ring-2 focus:ring-gray-500 z-30 top-0 right-12 mt-6`}
+              className={`block lg:hidden bg-[#D2E4F7] p-2 rounded-md text-gray-500 hover:text-gray-700 focus:text-gray-700 fixed focus:outline-none focus:ring-2 focus:ring-gray-500 z-30 top-0 right-12 mt-6`}
             >
               <svg
-                aria-label="close main menu"
                 xmlns="http://www.w3.org/2000/svg"
                 width={24}
                 height={24}
                 viewBox="0 0 24 24"
                 strokeWidth="1.5"
-                stroke="#2c3e50"
+                stroke="#184A7E"
                 fill="none"
                 strokeLinecap="round"
                 strokeLinejoin="round"
